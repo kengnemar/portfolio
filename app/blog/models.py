8a6e7f2f3071c8fa336807946c2257a447d4aefd
Marius KENGNE <mariuskengne@Mariuss-MacBook-Pro.local>
2022-08-11 19:14:53 -0500

    Fixed syntax errors for flake8 lint

--- app/blog/models.py
+++ app/blog/models.py
@@ -8,4 +8,3 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
-
--- app/blog/urls.py
+++ app/blog/urls.py
@@ -6,5 +6,4 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
     path('<int:blog_id>/', views.detail, name='detail'),
-
 ]
